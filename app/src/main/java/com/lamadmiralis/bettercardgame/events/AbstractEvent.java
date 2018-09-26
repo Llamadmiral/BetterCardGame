f2497d335d5f30aed12e7a9db3620195ac339054
Adam Maczak <Adam.Maczak@loxon.eu>
2018-09-26 10:03:17 +0200

    small modification

--- app/src/main/java/com/lamadmiralis/bettercardgame/events/AbstractEvent.java
+++ app/src/main/java/com/lamadmiralis/bettercardgame/events/AbstractEvent.java
@@ -1,21 +1,15 @@
 package com.lamadmiralis.bettercardgame.events;
 
-import java.util.Objects;
-
 /**
  * @author maczaka
  */
 public abstract class AbstractEvent {
-    private static long count = 0L;
 
     private long createTimeStamp = System.currentTimeMillis();
     private Long offset;
-    private long id;
 
     public AbstractEvent(final long offset) {
         this.offset = offset;
-        this.id = count;
-        count++;
     }
 
     public boolean isFireable() {
@@ -28,17 +22,4 @@ public abstract class AbstractEvent {
         return offset;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AbstractEvent that = (AbstractEvent) o;
-        return this.id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(id);
-    }
 }
